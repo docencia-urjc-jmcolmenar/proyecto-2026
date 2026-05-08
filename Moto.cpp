4adac3f80f394efd8b8edbe20dc198421a76f994
José Manuel (Chema) Colmenar <josemanuel.colmenar@urjc.es>
2026-05-08 11:07:26 +0200

    Constructor a partir de JSON Simplifica carga de objetos

--- Moto.cpp
+++ Moto.cpp
@@ -23,3 +23,22 @@ std::string Moto::aplanaObjeto() {
     json += "}";
     return json;
 }
+
+Moto::Moto(std::string cadenaJSON) {
+    // Crea el objeto a partir del parseo de la cadena JSON que se reciba.
+    // Para simplificar, vamos a suponer que la cadena JSON tiene el formato exacto que genera
+    // aplanaObjeto() y no vamos a hacer un parseo completo, sino una extracción de los valores
+    // entre comillas.
+    matricula = cadenaJSON.substr(cadenaJSON.find("\"matricula\": \"") + 14);
+    matricula = matricula.substr(0, matricula.find("\""));
+    marca = cadenaJSON.substr(cadenaJSON.find("\"marca\": \"") + 10);
+    marca = marca.substr(0, marca.find("\""));
+    modelo = cadenaJSON.substr(cadenaJSON.find("\"modelo\": \"") + 11);
+    modelo = modelo.substr(0, modelo.find("\""));
+    std::string kilometrajeStr = cadenaJSON.substr(cadenaJSON.find("\"kilometraje\": ") + 15);
+    kilometrajeStr = kilometrajeStr.substr(0, kilometrajeStr.find(","));
+    kilometraje = std::stol(kilometrajeStr);
+    std::string cilindradaStr = cadenaJSON.substr(cadenaJSON.find("\"cilindrada\": ") + 15);
+    cilindradaStr = cilindradaStr.substr(0, cilindradaStr.find("}"));
+    cilindrada = std::stoi(cilindradaStr);
+}
